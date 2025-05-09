752ffb4ca3b8f681e0c1c59dd569fbc87f4ee668
NourAlaa-BRUCE <nwrlanwrla2005@gmail.com>
2025-05-09 17:24:05 +0300

    Add income.java

---
+++ src/models/Income.java
@@ -0,0 +1,24 @@
+package src.models;
+
+import java.time.LocalDate;
+
+public class Income {
+    private String source;
+    private double amount;
+    private LocalDate date;
+
+    public Income(String source, double amount, LocalDate date) {
+        this.source = source;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public String getSource() { return source; }
+    public double getAmount()  { return amount;  }
+    public LocalDate getDate() { return date;    }
+
+    @Override
+    public String toString() {
+        return date + " | " + source + " : " + amount;
+    }
+}
